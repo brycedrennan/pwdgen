81fa4c1aa6e77fc6e266fd25ef7c29211a343358
Bryce Drennan <github@accounts.brycedrennan.com>
2017-07-27 16:47:31 -0700

    simplify test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,8 +3,7 @@ from pwdgen.rand import sample_with_replacement
 
 def test_sample_with_replacement_performance():
     population = ['1', '2', '3', '4', '5']
-    for _ in range(100):
-        sample = sample_with_replacement(population, 100)
+    sample = sample_with_replacement(population, 100)
 
     # selected all options
     assert len(set(sample)) == 5
